b40e3d2bb0d14d99968fc88d43d8e2d6cfc03ce3
549naim <swenaim35549@gmail.com>
2025-07-21 12:12:48 +0600

    first_push

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+ <script src="{{ asset('dashboard/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/js/app.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/libs/simplebar/dist/simplebar.js') }}"></script>
+
+    <!-- solar icons -->
+    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
