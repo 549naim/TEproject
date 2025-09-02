2862c2e0f69e079f01194c57e9ac80ca1cdb3eb7
549naim <swenaim35549@gmail.com>
2025-09-02 20:52:33 +0600

    text_fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,7 +24,7 @@
         @yield('content')
     </div>
    
-<footer class="w-100 bg-white mt-5 py-2 px-3 {{ request()->is('evaluation/student') ? 'position-fixed bottom-0' : '' }}">
+<footer class="w-100 bg-white mt-5 py-2 px-3">
     <div class="text-center">
         <span class="text-muted">
             © {{ date('Y') }} All rights reserved <br>
